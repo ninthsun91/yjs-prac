d63df66d36da8042ae5314f69309f9dc96e4ef82
Ray Kim <ninthsun91@gmail.com>
2024-05-24 19:43:38 +0900

    ✨ Observe ymap changes

--- src/components/Whiteboard.tsx
+++ src/components/Whiteboard.tsx
@@ -10,11 +10,20 @@ import type { ExcalidrawElement } from '@excalidraw/excalidraw/types/element/typ
 
 export function Whiteboard() {
   const [excalidrawAPI, setExcalidrawAPI] = useState<ExcalidrawImperativeAPI>(null!);
+  const [initialized, setInitialized] = useState(false);
   const context = useDocContext();
 
   const init = async (api: ExcalidrawImperativeAPI) => {
     setExcalidrawAPI(api);
-  }
+  
+    if (!context) return;
+
+    const ymap = context.doc.getMap('map');
+    const elements = ymap.get('elements') as ExcalidrawElement[];
+    if (elements) {
+      api.updateScene({ elements });
+    }
+  };
 
   const onChangeHandler = (elements: readonly ExcalidrawElement[], state: AppState, files: BinaryFiles) => {
     if (!context) {
@@ -24,12 +33,13 @@ export function Whiteboard() {
     const ymap = context.doc.getMap('map');
     ymap.set('elements', elements);
     ymap.set('files', files);
+
     // console.log("onChange", elements, state, files);
     // if (state.activeTool.type === 'image' && state.cursorButton === 'down') {
     //   const files = excalidrawAPI.getFiles();
     //   console.log("files added", files);
     // }
-  }
+  };
   
   useWindowEventListener('keydown', (e) => {
     const isCtrlOrCmd = e.metaKey || e.ctrlKey;
@@ -46,6 +56,18 @@ export function Whiteboard() {
 
     context.ws.connect();
 
+    const ymap = context.doc.getMap('map');
+    ymap.observe((event) => {
+      // console.log('ymap changed', event);
+      const elements = ymap.get('elements') as ExcalidrawElement[];
+      console.log('ymap observe: elements', elements);
+      if (elements && excalidrawAPI) {
+        excalidrawAPI.updateScene({ elements });
+      }
+    });
+
+    setInitialized(true);
+
     return () => {
       context.ws.disconnect();
     };
@@ -53,7 +75,12 @@ export function Whiteboard() {
   
   return (
     <div className="border border-red-500 h-screen">
-      <Excalidraw excalidrawAPI={init} onChange={onChangeHandler} />
+      {initialized && (
+        <Excalidraw
+          excalidrawAPI={init}
+          onChange={onChangeHandler}
+        />
+      )}
     </div>
   );
 }
