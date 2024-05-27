28d3bcaaa7043f8d4c8974a360dc77e21fa51fba
Ray Kim <ninthsun91@gmail.com>
2024-05-27 21:20:36 +0900

    ✨ Connect to socket server and sync whiteboard

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,75 @@
 import { Server } from 'socket.io'
 
-const io = new Server()
+import * as encoding from 'lib0/encoding';
+import * as decoding from 'lib0/decoding';
 
-io.on('connection', (socket) => {
-  console.log('connection: ', socket.id)
+import type { ExcalidrawElement } from '@excalidraw/excalidraw/types/element/types';
+import type { BinaryFiles } from '@excalidraw/excalidraw/types/types';
+
+type ProjectID = string;
+interface WhiteboardData {
+  elements: ExcalidrawElement[];
+  files: BinaryFiles;
+}
+
+class DB {
+  constructor(
+    private readonly db: Map<ProjectID, WhiteboardData>,
+  ) {}
+
+  public get(roomID: ProjectID): WhiteboardData {
+    const data = this.db.get(roomID);
+    if (data) return data;
+
+    const newData: WhiteboardData = {
+      elements: [],
+      files: {},
+    };
+    this.db.set(roomID, newData);
+    return newData;
+  }
+}
+
+const io = new Server({
+  cors: {
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  }
 })
 
-io.listen(3000)
+const db = new DB(new Map());
+
+io.on('connection', (socket) => {
+  // Join room with projectID
+  // and send whiteboard data back to client
+  const { projectId } = socket.handshake.query;
+  if (typeof projectId !== 'string') {
+    console.log('disconnecting: no projectId');
+    socket.disconnect();
+    return;
+  }
+
+  console.log('connection: ', socket.id);
+
+  socket.join(projectId);
+
+  const data = db.get(projectId);
+  socket.emit('sync', encodeData(data));
+
+  socket.on('update', (data: any) => {
+    console.log('update: ', data);
+  })
+});
+
+io.listen(3333);
+
+function encodeData(data: WhiteboardData): Uint8Array {
+  const encoder = encoding.createEncoder();
+  encoding.writeAny(encoder, data);
+  return encoding.toUint8Array(encoder);
+}
+
+function decodeData(buffer: Uint8Array): WhiteboardData {
+  const decoder = decoding.createDecoder(buffer);
+  return decoding.readAny(decoder);
+}
--- src/components/Whiteboard2.tsx
+++ src/components/Whiteboard2.tsx
@@ -2,10 +2,31 @@
 
 import { useEffect, useState } from 'react';
 import { Excalidraw } from '@excalidraw/excalidraw';
+import { io, Socket } from 'socket.io-client';
+
+import * as encoding from 'lib0/encoding';
+import * as decoding from 'lib0/decoding';
+
 import { useWindowEventListener } from '@/hooks/useWindowEventListener';
 
 import type { AppState, BinaryFiles, ExcalidrawImperativeAPI } from '@excalidraw/excalidraw/types/types';
 import type { ExcalidrawElement } from '@excalidraw/excalidraw/types/element/types';
+
+interface WhiteboardData {
+  elements: ExcalidrawElement[];
+  files: BinaryFiles;
+}
+
+const socket = io('http://localhost:3333', {
+  auth: {
+    token: 'auth token',
+  },
+  query: {
+    projectId: 'project-id-1234',
+  },
+  withCredentials: true,
+  autoConnect: false,
+});
 
 export function Whiteboard2() {
   const [excalidrawAPI, setExcalidrawAPI] = useState<ExcalidrawImperativeAPI>(null!);
@@ -13,6 +34,7 @@ export function Whiteboard2() {
 
   const init = async (api: ExcalidrawImperativeAPI) => {
     setExcalidrawAPI(api);
+    setInitialized(true);
   };
 
   const onChangeHandler = (elements: readonly ExcalidrawElement[], state: AppState, files: BinaryFiles) => {
@@ -30,24 +52,50 @@ export function Whiteboard2() {
         const files = excalidrawAPI.getFiles();
         console.log("files pasted", files);
       }, 500);
-    } 
+    }
   });
 
-  useEffect(() => {
-    setInitialized(true);
+  const addSocketListeners = (socket: Socket) => {
+    socket.on('connect', () => {
+      console.log('connected to socket server', socket.id);
+    });
 
+    socket.on('sync', (data: any) => {
+      console.log('sync', decodeData(data));
+      excalidrawAPI.updateScene(decodeData(data));
+    })
+  }
+
+  useEffect(() => {
     return () => {
+      if (socket.connected) socket.disconnect();
     };
   }, []);
+
+  useEffect(() => {
+    if (!initialized) return;
+
+    socket.connect();
+    addSocketListeners(socket);
+  }, [initialized]);
   
   return (
     <div className="border border-red-500 h-screen">
-      {initialized && (
-        <Excalidraw
-          excalidrawAPI={init}
-          onChange={onChangeHandler}
-        />
-      )}
+      <Excalidraw
+        excalidrawAPI={init}
+        onChange={onChangeHandler}
+      />
     </div>
   );
+}
+
+function decodeData(data: ArrayBuffer): WhiteboardData {
+  const decoder = decoding.createDecoder(new Uint8Array(data));
+  return decoding.readAny(decoder);
+}
+
+function encodeData(data: WhiteboardData): Uint8Array {
+  const encoder = encoding.createEncoder();
+  encoding.writeAny(encoder, data);
+  return encoding.toUint8Array(encoder);
 }
