3e3f50fde871e70737e6a4b1d5167c562a7d0df5
Ray Kim <ninthsun91@gmail.com>
2024-06-13 22:31:42 +0900

    ♻️ Wrap socket.io related codes to custom hook

--- src/components/Whiteboard2.tsx
+++ src/components/Whiteboard2.tsx
@@ -2,35 +2,19 @@
 
 import { useCallback, useEffect } from 'react'
 import { Excalidraw } from '@excalidraw/excalidraw'
-import { io, Socket } from 'socket.io-client'
-
-import * as encoding from 'lib0/encoding'
-import * as decoding from 'lib0/decoding'
 
 import { useCallbackRefState } from '@/hooks/useCallbackStateRef'
 import { useWindowEventListener } from '@/hooks/useWindowEventListener'
+import { useSocketio } from '@/hooks/useSocketio'
 
 import type { AppState, BinaryFiles, ExcalidrawImperativeAPI } from '@excalidraw/excalidraw/types/types'
 import type { ExcalidrawElement } from '@excalidraw/excalidraw/types/element/types'
 
-interface WhiteboardData {
-  elements: readonly ExcalidrawElement[]
-  files: BinaryFiles
-}
+const projectId = 'project-id';
 
-const socket = io('http://localhost:3333', {
-  auth: {
-    token: 'auth token'
-  },
-  query: {
-    projectId: 'project-id-1234'
-  },
-  withCredentials: true,
-  autoConnect: false
-})
-
-export function Whiteboard2 () {
+export function Whiteboard2() {
   const [excalidrawAPI, excalidrawRefCallback] = useCallbackRefState<ExcalidrawImperativeAPI>()
+  const { isConnected, listenSync, update } = useSocketio(projectId)
 
   const onChangeHandler = (elements: readonly ExcalidrawElement[], state: AppState, files: BinaryFiles) => { }
 
@@ -49,36 +33,18 @@ export function Whiteboard2 () {
     e.preventDefault()
     if (excalidrawAPI == null) return
     const elements = excalidrawAPI.getSceneElements()
-    const files = excalidrawAPI.getFiles()
-    console.log('update', elements)
-    socket.emit('update', encodeData({ elements, files }))
+    update(elements)
   })
 
-  const addSocketListeners = useCallback((socket: Socket) => {
-    socket.on('connect', () => {
-      console.log('connected to socket server', socket.id)
-    })
-
-    socket.on('sync', (buffer: ArrayBuffer) => {
-      const data = decodeData(buffer)
-      const { elements, appState } = reconcileData(excalidrawAPI!, data)
-      excalidrawAPI!.updateScene({ elements, appState })
-      if (data.files) excalidrawAPI!.addFiles(Object.values(data.files))
-    })
-  }, [excalidrawAPI])
+  const addSocketListeners = useCallback(() => {
+    listenSync(excalidrawAPI!)
+  }, [excalidrawAPI, listenSync])
 
   useEffect(() => {
-    return () => {
-      if (socket.connected) socket.disconnect()
-    }
-  }, [])
+    if (excalidrawAPI == null || !isConnected) return
 
-  useEffect(() => {
-    if (excalidrawAPI == null) return
-
-    socket.connect()
-    addSocketListeners(socket)
-  }, [excalidrawAPI, addSocketListeners])
+    addSocketListeners()
+  }, [excalidrawAPI, isConnected, addSocketListeners])
 
   return (
     <div className='border border-red-500 h-screen'>
@@ -90,49 +56,3 @@ export function Whiteboard2 () {
   )
 }
 
-function decodeData (data: ArrayBuffer): WhiteboardData {
-  const decoder = decoding.createDecoder(new Uint8Array(data))
-  return decoding.readAny(decoder)
-}
-
-function encodeData (data: WhiteboardData): Uint8Array {
-  const encoder = encoding.createEncoder()
-  encoding.writeAny(encoder, data)
-  return encoding.toUint8Array(encoder)
-}
-
-function reconcileData (api: ExcalidrawImperativeAPI, remoteData: WhiteboardData) {
-  const localData = api.getSceneElementsIncludingDeleted()
-  const localState = api.getAppState()
-
-  const localElementMap = arrayToMap(localData)
-  remoteData.elements.forEach((element) => {
-    if (localElementMap.has(element.id)) {
-      const localElement = localElementMap.get(element.id)!
-      localElementMap.set(element.id, convergeElements(localElement, element))
-    } else {
-      localElementMap.set(element.id, element)
-    }
-  })
-
-  const reconciledElements = [...localElementMap.values()]
-
-  return {
-    elements: reconciledElements,
-    appState: localState
-  }
-}
-
-function arrayToMap (elements: readonly ExcalidrawElement[]): Map<string, ExcalidrawElement> {
-  return elements.reduce((acc, element) => {
-    acc.set(element.id, element)
-    return acc
-  }, new Map<string, ExcalidrawElement>())
-}
-
-function convergeElements (local: ExcalidrawElement, remote: ExcalidrawElement): ExcalidrawElement {
-  if (local.version > remote.version) return local
-  if (remote.version > local.version) return remote
-  if (local.versionNonce <= remote.versionNonce) return local
-  return remote
-}
---
+++ src/hooks/useSocketio.ts
@@ -0,0 +1,107 @@
+import { useEffect, useState } from 'react'
+import { Socket, io } from 'socket.io-client'
+import * as encoding from 'lib0/encoding'
+import * as decoding from 'lib0/decoding'
+import type { AppState, BinaryFiles, ExcalidrawImperativeAPI } from '@excalidraw/excalidraw/types/types'
+import type { ExcalidrawElement } from '@excalidraw/excalidraw/types/element/types'
+
+const CONNECTION_URL = 'http://localhost:3333'
+
+type WhiteboardData = {
+  elements: readonly ExcalidrawElement[]
+}
+
+export const useSocketio = (projectId: string) => {
+  const [socket, setSocket] = useState<Socket>(null!);
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+
+  const update = (elements: readonly ExcalidrawElement[]) => {
+    socket.emit('update', encodeData({ elements }))
+  }
+
+  const listenSync = (api: ExcalidrawImperativeAPI) => {
+    socket.on('sync', (buffer: ArrayBuffer) => {
+      const data = decodeData(buffer)
+      const { elements, appState } = reconcileData(api, data)
+      api.updateScene({ elements, appState })
+    })
+  }
+
+  useEffect(() => {
+    const socket = io(CONNECTION_URL, {
+      auth: {
+        token: 'auth token'
+      },
+      query: { projectId },
+      withCredentials: true,
+      autoConnect: true
+    })
+    socket.on('connect', () => {
+      console.log('socket.io connected', socket.id)
+      setIsConnected(true)
+    })
+    setSocket(socket);
+
+    return () => {
+      if (socket.connected) {
+        socket.disconnect();
+        console.log('socket.io disconnected', socket.id, socket.disconnected)
+        setIsConnected(false)
+      }
+    }
+  }, [projectId])
+
+  return {
+    update,
+    listenSync,
+    isConnected
+  }
+}
+
+function decodeData(data: ArrayBuffer): WhiteboardData {
+  const decoder = decoding.createDecoder(new Uint8Array(data))
+  return decoding.readAny(decoder)
+}
+
+function encodeData(data: WhiteboardData): Uint8Array {
+  const encoder = encoding.createEncoder()
+  encoding.writeAny(encoder, data)
+  return encoding.toUint8Array(encoder)
+}
+
+function reconcileData(api: ExcalidrawImperativeAPI, remoteData: WhiteboardData) {
+  const localData = api.getSceneElementsIncludingDeleted()
+  const localState = api.getAppState()
+
+  const localElementMap = arrayToMap(localData)
+  remoteData.elements.forEach((element) => {
+    if (localElementMap.has(element.id)) {
+      const localElement = localElementMap.get(element.id)!
+      localElementMap.set(element.id, convergeElements(localElement, element))
+    } else {
+      localElementMap.set(element.id, element)
+    }
+  })
+
+  const reconciledElements = [...localElementMap.values()]
+
+  return {
+    elements: reconciledElements,
+    appState: localState
+  }
+}
+
+function arrayToMap(elements: readonly ExcalidrawElement[]): Map<string, ExcalidrawElement> {
+  return elements.reduce((acc, element) => {
+    acc.set(element.id, element)
+    return acc
+  }, new Map<string, ExcalidrawElement>())
+}
+
+function convergeElements(local: ExcalidrawElement, remote: ExcalidrawElement): ExcalidrawElement {
+  if (local.version > remote.version) return local
+  if (remote.version > local.version) return remote
+  if (local.versionNonce <= remote.versionNonce) return local
+  return remote
+}
+
